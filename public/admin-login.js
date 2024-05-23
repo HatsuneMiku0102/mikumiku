600736b979166e421a32ddcb040c5781de21c51c
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-23 16:02:50 +0100

    Update admin-login.js

--- public/admin-login.js
+++ public/admin-login.js
@@ -4,8 +4,8 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Basic authentication (in a real-world application, use server-side authentication)
-    if (username === 'admin' && password === 'password') {
+    if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+        localStorage.setItem('auth', 'true');
         window.location.href = 'admin-dashboard.html';
     } else {
         document.getElementById('error-message').textContent = 'Invalid username or password';
