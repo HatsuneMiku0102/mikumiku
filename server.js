e96315d27bb2377741a8d523b2b18d566f8cc839
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-23 16:23:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
 const users = [
     {
@@ -70,6 +72,11 @@ app.get('/api/videos', (req, res) => {
 
         res.json(JSON.parse(data));
     });
+});
+
+// Handle requests to the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
