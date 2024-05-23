52056e4df1a669686a77cd00b76ceb1408fca71c
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-23 21:44:17 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,25 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-// Read environment variables
-const adminUsername = process.env.ADMIN_USERNAME;
-const adminPassword = process.env.ADMIN_PASSWORD;
-
-// Read the admin login HTML file
+// Path to the admin login HTML file
 const adminLoginHtmlPath = path.join(__dirname, 'public', 'admin-login.html');
+
+// Read the admin login HTML file (no modification of environment variables)
 let adminLoginHtml = fs.readFileSync(adminLoginHtmlPath, 'utf8');
 
-// Inject the environment variables into the HTML file
-adminLoginHtml = adminLoginHtml.replace(
-    '<script src="admin-login.js"></script>',
-    `<script>
-        const ADMIN_USERNAME = "${adminUsername}";
-        const ADMIN_PASSWORD = "${adminPassword}";
-    </script>
-    <script src="admin-login.js"></script>`
-);
+// Optionally, you can perform other build steps here if needed
 
-// Write the modified HTML file back to the filesystem
+// Write the (potentially modified) HTML file back to the filesystem
 fs.writeFileSync(adminLoginHtmlPath, adminLoginHtml, 'utf8');
 
-console.log('Admin login HTML file has been updated with environment variables.');
+console.log('Admin login HTML file has been processed.');
