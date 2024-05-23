59eb0a9ef1fdd7453f0e42dcf9575b934b1463c6
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-23 16:01:40 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read environment variables
+const adminUsername = process.env.ADMIN_USERNAME;
+const adminPassword = process.env.ADMIN_PASSWORD;
+
+// Read the admin login HTML file
+const adminLoginHtmlPath = path.join(__dirname, 'public', 'admin-login.html');
+let adminLoginHtml = fs.readFileSync(adminLoginHtmlPath, 'utf8');
+
+// Inject the environment variables into the HTML file
+adminLoginHtml = adminLoginHtml.replace(
+    '<script src="admin-login.js"></script>',
+    `<script>
+        const ADMIN_USERNAME = "${adminUsername}";
+        const ADMIN_PASSWORD = "${adminPassword}";
+    </script>
+    <script src="admin-login.js"></script>`
+);
+
+// Write the modified HTML file back to the filesystem
+fs.writeFileSync(adminLoginHtmlPath, adminLoginHtml, 'utf8');
+
+console.log('Admin login HTML file has been updated with environment variables.');
 
