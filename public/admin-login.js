15d9c0c5f6239a02c23c43db44c8bb169d7dd2f1
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-23 20:08:19 +0100

    Update admin-login.js

--- public/admin-login.js
+++ public/admin-login.js
@@ -11,17 +11,17 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         },
         body: JSON.stringify({ username, password })
     })
-    .then(response => response.json().then(data => {
-        if (!response.ok) {
-            console.error('Login failed:', data);
-            document.getElementById('error-message').textContent = data.message || 'Invalid username or password';
-        } else {
+    .then(response => response.json())
+    .then(data => {
+        if (data.auth) {
             localStorage.setItem('token', data.token);
             window.location.href = 'admin-dashboard.html';
+        } else {
+            document.getElementById('error-message').textContent = 'Invalid username or password';
         }
-    }))
+    })
     .catch(error => {
         console.error('Error:', error);
-        document.getElementById('error-message').textContent = 'Login failed';
+        document.getElementById('error-message').textContent = 'An error occurred. Please try again.';
     });
 });
