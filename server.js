dfe8e1e4bbca6c54ad9ca03a43c8ac82460ea9ea
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-24 00:17:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'your-secret-key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true } // set true if using https
+    cookie: { secure: true, sameSite: 'strict' } 
 }));
 
 // Serve static files from the "public" directory
