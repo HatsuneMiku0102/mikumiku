8c2b6d600f3f87f26b35f33f61a02b527fdb83d3
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-05-24 04:08:49 +0100

    Update typing-effect.js

--- public/typing-effect.js
+++ public/typing-effect.js
@@ -1,22 +1,41 @@
+<script>
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('typing-text');
     const text = 'MikuMiku';
     const delayBetweenWords = 500; 
+    const typingSpeed = 200; 
 
     let index = 0;
+    let currentWord = '';
+
+    function typeCharacter() {
+        if (index < text.length) {
+            currentWord += text[index];
+            textElement.innerHTML = currentWord;
+            index++;
+            setTimeout(typeCharacter, typingSpeed);
+        } else if (index === text.length) {
+            setTimeout(() => {
+                textElement.innerHTML = currentWord + ' ';
+                index++;
+                setTimeout(typeWord, delayBetweenWords);
+            }, typingSpeed);
+        }
+    }
 
     function typeWord() {
         if (index < text.length) {
-            let currentWord = '';
+            currentWord = '';
             while (index < text.length && text[index] !== ' ') {
                 currentWord += text[index];
                 index++;
             }
             index++; 
-            textElement.innerHTML += currentWord + ' ';
-            setTimeout(typeWord, delayBetweenWords);
+            textElement.innerHTML = currentWord + ' ';
+            setTimeout(typeCharacter, delayBetweenWords);
         }
     }
 
     typeWord();
 });
+</script>
