7faf53e3f76561a25a2ec66f2271c8247143090e
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-06-01 11:44:21 +0100

    Update admin-login.js

--- public/admin-login.js
+++ public/admin-login.js
@@ -1,25 +1,29 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.auth) {
-            window.location.href = 'admin-dashboard.html';
-        } else {
-            document.getElementById('error-message').textContent = 'Invalid username or password';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('error-message').textContent = 'An error occurred. Please try again.';
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.auth) {
+                window.location.href = 'admin-dashboard.html';
+            } else {
+                document.getElementById('error-message').textContent = 'Invalid username or password';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('error-message').textContent = 'An error occurred. Please try again.';
+        });
     });
 });
