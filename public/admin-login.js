1427a82e2aac7bfe2c1cf03c26cebd06fd156075
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-06-01 11:49:56 +0100

    Update admin-login.js

--- public/admin-login.js
+++ public/admin-login.js
@@ -1,29 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
 
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-
-        fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.auth) {
-                window.location.href = 'admin-dashboard.html';
-            } else {
-                document.getElementById('error-message').textContent = 'Invalid username or password';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('error-message').textContent = 'An error occurred. Please try again.';
+            fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.auth) {
+                    localStorage.setItem('token', data.token);
+                    window.location.href = 'admin-dashboard.html';
+                } else {
+                    document.getElementById('error-message').textContent = 'Invalid username or password';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById('error-message').textContent = 'An error occurred. Please try again.';
+            });
         });
-    });
+    }
 });
