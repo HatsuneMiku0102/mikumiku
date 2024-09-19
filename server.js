6c287c9b5323e2bf2bbaefb7baa7a54268f86641
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-09-19 01:29:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ const app = express();
 const server = http.createServer(app); // Create an HTTP server for Socket.IO
 const io = socketIo(server, {
     cors: {
-        origin: "https://mikumiku.dev", // Replace with your client domain
-        methods: ["GET", "POST"]
+        origin: "chrome-extension://<ealgoodedcojbceodddhbpcklnpneocp>", // Replace with your extension ID
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
     }
 });
 
