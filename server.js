707c7a638252461f72a826356ce6b1505ab32b7d
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-09-30 17:00:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -73,25 +73,7 @@ app.use(cookieParser());
 app.use(cors());
 
 
-const categoryMappings = {
-    "1": "Film & Animation",
-    "2": "Autos & Vehicles",
-    "10": "Music",
-    "15": "Pets & Animals",
-    "17": "Sports",
-    "19": "Travel & Events",
-    "20": "Gaming",
-    "21": "Videoblogging",
-    "22": "People & Blogs",
-    "23": "Comedy",
-    "24": "Entertainment",
-    "25": "News & Politics",
-    "26": "Howto & Style",
-    "27": "Education",
-    "28": "Science & Technology",
-    "29": "Nonprofits & Activism",
-    // Add more mappings as necessary
-};
+
 
 // Update Video or Browsing Presence Data
 const categoryMappings = {
