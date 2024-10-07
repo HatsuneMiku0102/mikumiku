1d98af3be8715e0723b2c45378811892db4ef16a
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2024-10-07 08:59:53 +0100

    Update generateHash.js

--- generateHash.js
+++ generateHash.js
@@ -1,13 +1,18 @@
 // generateHash.js
 const bcrypt = require('bcryptjs');
 
-const adminPassword = 'CV01'; // Replace with your desired password
-const saltRounds = 12; // You can adjust the salt rounds as needed
+// Replace this with your desired admin password
+const adminPassword = 'CV01';
+
+// Number of salt rounds (higher is more secure but slower)
+const saltRounds = 12;
 
 bcrypt.hash(adminPassword, saltRounds, (err, hash) => {
     if (err) {
         console.error('Error hashing password:', err);
+        process.exit(1);
     } else {
         console.log('Hashed Password:', hash);
+        process.exit(0);
     }
 });
