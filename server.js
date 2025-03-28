bc7067ef3922e5df84b49755fb64d7ae75483a1e
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2025-03-28 10:13:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1466,7 +1466,7 @@ if (!process.env.MONGO_URL) {
   console.error("Error: MONGO_URL environment variable not set.");
   process.exit(1);
 }
-const mongoUrl = process.env.MONGO_URL;
+
 const dbName = process.env.MONGO_DB_NAME || "myfirstdatabase";
 
 const client = new MongoClient(mongoUrl, { useUnifiedTopology: true });
