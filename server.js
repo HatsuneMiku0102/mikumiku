7e69cceaa869987f7642f592a27b631389b3d61f
HatsuneMiku0102 <hystoriyaallusiataylor@gmail.com>
2025-03-30 13:00:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1467,7 +1467,7 @@ if (!process.env.MONGO_URL) {
   process.exit(1);
 }
 
-const mongoUrl = process.env.MONGO_URL;
+
 const dbName = process.env.MONGO_DB_NAME || "myfirstdatabase";
 
 const client = new MongoClient(mongoUrl, { useUnifiedTopology: true });
